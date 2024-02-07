d9faef5611db6002d91e112e166a8a67f14a17b3
rajabirla <raja.birla2002@gmail.com>
2024-02-07 12:46:45 +0530

    variables in js

---
+++ 01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 133434;
+let accoutnEmail = "raja.birla2002@gmail.com"
+var accountPassword = "1234"
+accountCity = "Indore"
+let accountState;
+// accountId = 2;  not allowed in the case of const
+// console.log(accountId);
+
+
+// accoutnEmail = "rajanandlalbirla@gmail.com" // This is allowed in js where we can override the value of let
+// console.log(accoutnEmail);
+
+
+accoutnEmail = "rajanandlalbirla@gmail.com"
+accountPassword = "33323343"
+accountCity = "Delhi"
+
+console.table([accountId,accountPassword, accoutnEmail, accountCity, accountState])
