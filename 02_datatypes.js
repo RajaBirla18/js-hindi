bbfa3b29c305c51d588510e4ab460739c87211e4
rajabirla <raja.birla2002@gmail.com>
2024-02-07 13:38:44 +0530

    datatypes in javascript

---
+++ 02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all js code as newer version
+// code readability should be high in js or any programming language so write the clean code 
+
+
+
+// console.log(3 + 3); 
+
+// console.log("raja birla"); // this is a good practice to write the code 
+
+
+let name = "Raja Birla"; // like a string 
+let age = 22; // a number 
+let isLoggedIn = false; // it is a boolean value
+
+
+// Premitive datatypes
+
+// number => 2 to power 53
+// string => "value"
+// bigint => bit number generally used in big website like facebook, google and instagram
+// boolean => true or false
+// null => standalone value
+// undefined => value to be not assigned
+// symbol => to find the unique value in js , we use symbol most in react.js to find the value
+
+
+// Non-premitive datatypes
+
+// Array 
+// object
+
+
+
